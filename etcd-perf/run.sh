ca2c634e173a203fc999ff155d93d0ecf64a8403
Raul Sevilla <rsevilla@redhat.com>
2022-09-12 13:33:41 +0200

    Increase block size, use runtime based test and update pass/fail criteria to 20ms
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- etcd-perf/run.sh
+++ etcd-perf/run.sh
@@ -7,7 +7,7 @@ mkdir -p /var/lib/etcd
 
 # Run fio
 echo "---------------------------------------------------------------- Running fio ---------------------------------------------------------------------------"
-fio --rw=write --ioengine=sync --fdatasync=1 --directory=/var/lib/etcd --size=22m --bs=2300 --name=etcd_perf --output-format=json | tee /tmp/fio.out
+fio --rw=write --ioengine=sync --fdatasync=1 --directory=/var/lib/etcd --size=100m --bs=7500 --name=etcd_perf --output-format=json --runtime=60 --time_based=1 | tee /tmp/fio.out
 echo "--------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 # Scrape the fio output for p99 of fsync in ns
@@ -15,8 +15,8 @@ fsync=$(cat /tmp/fio.out | jq '.jobs[0].sync.lat_ns.percentile["99.000000"]')
 echo "99th percentile of fsync is $fsync ns"
 
 # Compare against the recommended value
-if [[ $fsync -ge 10000000 ]]; then
-  echo "99th percentile of the fsync is greater than the recommended value which is 10 ms, faster disks are recommended to host etcd for better performance"
+if [[ $fsync -ge 20000000 ]]; then
+  echo "99th percentile of the fsync is greater than the recommended value which is 20 ms, faster disks are recommended to host etcd for better performance"
 else
-  echo "99th percentile of the fsync is within the recommended threshold - 10 ms, the disk can be used to host etcd"
+  echo "99th percentile of the fsync is within the recommended threshold: - 20 ms, the disk can be used to host etcd"
 fi

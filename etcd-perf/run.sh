b37d90782c49990e20d9690178dcd3d3fbc4cf12
Raul Sevilla <rsevilla@redhat.com>
2022-09-12 13:34:42 +0200

    Add multi-arch build makefile
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- etcd-perf/run.sh
+++ etcd-perf/run.sh
@@ -7,7 +7,7 @@ mkdir -p /var/lib/etcd
 
 # Run fio
 echo "---------------------------------------------------------------- Running fio ---------------------------------------------------------------------------"
-fio --rw=write --ioengine=sync --fdatasync=1 --directory=/var/lib/etcd --size=100m --bs=7500 --name=etcd_perf --output-format=json --runtime=60 --time_based=1 | tee /tmp/fio.out
+fio --rw=write --ioengine=sync --fdatasync=1 --directory=/var/lib/etcd --size=100m --bs=8000 --name=etcd_perf --output-format=json --runtime=60 --time_based=1 | tee /tmp/fio.out
 echo "--------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 # Scrape the fio output for p99 of fsync in ns
@@ -16,7 +16,7 @@ echo "99th percentile of fsync is $fsync ns"
 
 # Compare against the recommended value
 if [[ $fsync -ge 20000000 ]]; then
-  echo "99th percentile of the fsync is greater than the recommended value which is 20 ms, faster disks are recommended to host etcd for better performance"
+  echo "99th percentile of the fsync is greater than the recommended value which is ${fsync} ns > 20 ms, faster disks are recommended to host etcd for better performance"
 else
   echo "99th percentile of the fsync is within the recommended threshold: - 20 ms, the disk can be used to host etcd"
 fi

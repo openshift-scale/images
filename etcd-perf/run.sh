68fb78569feb3e7623b22d5c53b66202e04ff2a5
Raul Sevilla <rsevilla@redhat.com>
2022-09-19 14:26:51 +0200

    Add loglevel
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- etcd-perf/run.sh
+++ etcd-perf/run.sh
@@ -12,11 +12,11 @@ echo "--------------------------------------------------------------------------
 
 # Scrape the fio output for p99 of fsync in ns
 fsync=$(cat /tmp/fio.out | jq '.jobs[0].sync.lat_ns.percentile["99.000000"]')
-echo "99th percentile of fsync is $fsync ns"
+echo "INFO: 99th percentile of fsync is $fsync ns"
 
 # Compare against the recommended value
 if [[ $fsync -ge 20000000 ]]; then
-  echo "99th percentile of the fsync is greater than the recommended value which is ${fsync} ns > 20 ms, faster disks are recommended to host etcd for better performance"
+  echo "WARN: 99th percentile of the fsync is greater than the recommended value which is ${fsync} ns > 20 ms, faster disks are recommended to host etcd for better performance"
 else
-  echo "99th percentile of the fsync is within the recommended threshold: - 20 ms, the disk can be used to host etcd"
+  echo "INFO: 99th percentile of the fsync is within the recommended threshold: - 20 ms, the disk can be used to host etcd"
 fi
